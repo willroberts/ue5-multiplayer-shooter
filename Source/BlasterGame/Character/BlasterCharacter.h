baf5ab1c14e65791f4205364c138d1e88822191a
Will Roberts <willroberts3@protonmail.com>
2023-03-16 12:54:17 -0400

    Adds camera and swing arm

--- Source/BlasterGame/Character/BlasterCharacter.h
+++ Source/BlasterGame/Character/BlasterCharacter.h
@@ -12,18 +12,22 @@ class BLASTERGAME_API ABlasterCharacter : public ACharacter
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this character's properties
 	ABlasterCharacter();
 
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
-	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(VisibleAnywhere, Category = Camera)
+	class USpringArmComponent* CameraBoom;
+
+	UPROPERTY(VisibleAnywhere, Category = Camera)
+	class UCameraComponent* FollowCamera;
+
+public:
 
 };
