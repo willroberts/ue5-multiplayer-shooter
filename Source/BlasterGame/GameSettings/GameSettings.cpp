5378fc90c5ca5bc5a6356f325d2f0118051f24c4
Will Roberts <willroberts3@protonmail.com>
2023-05-09 12:33:04 -0400

    Fills out GameSettings class

--- Source/BlasterGame/GameSettings/GameSettings.cpp
+++ Source/BlasterGame/GameSettings/GameSettings.cpp
@@ -1,3 +1,5 @@
 // (c) 2023 Will Roberts
 
 #include "GameSettings.h"
+
+UGameSettings::UGameSettings() {}
--- Source/BlasterGame/GameSettings/GameSettings.h
+++ Source/BlasterGame/GameSettings/GameSettings.h
@@ -6,8 +6,25 @@
 #include "Engine/DeveloperSettingsBackedByCVars.h"
 #include "GameSettings.generated.h"
 
-UCLASS()
+UCLASS(Config=Engine)
 class BLASTERGAME_API UGameSettings : public UDeveloperSettingsBackedByCVars
 {
 	GENERATED_BODY()
+
+public:
+	UGameSettings();
+
+	/* Game Settings */
+
+	UPROPERTY(EditAnywhere, Config, Category = BlasterGame, meta = (ConsoleVariable = "r.DynamicGlobalIlluminationMethod"))
+	bool bLumenEnabled = true;
+
+	UPROPERTY(EditAnywhere, Config, Category = BlasterGame, meta = (ConsoleVariable = "r.Nanite"))
+	bool bNaniteEnabled = true;
+
+	UPROPERTY(EditAnywhere, Config, Category = BlasterGame, meta = (ConsoleVariable = "r.Shadow.Virtual.Enable"))
+	bool bVirtualShadowMapsEnabled = true;
+
+	UPROPERTY(EditAnywhere, Config, Category = BlasterGame)
+	float GlobalVolumeMultiplier = 0.50f;
 };
