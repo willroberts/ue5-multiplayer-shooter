5e4412a284737a9c11d5564a79cf9e319bf5dc9b
Will Roberts <willroberts3@protonmail.com>
2023-05-09 12:15:36 -0400

    Adds GameSettings class

---
+++ Source/BlasterGame/GameSettings/GameSettings.h
@@ -0,0 +1,13 @@
+// (c) 2023 Will Roberts
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DeveloperSettingsBackedByCVars.h"
+#include "GameSettings.generated.h"
+
+UCLASS()
+class BLASTERGAME_API UGameSettings : public UDeveloperSettingsBackedByCVars
+{
+	GENERATED_BODY()
+};
