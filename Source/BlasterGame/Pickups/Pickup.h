8c06c00870f05828c0e8e10d22b11f2387cba637
Will Roberts <willroberts3@protonmail.com>
2023-06-19 11:35:40 -0400

    Adds pickup class

---
+++ Source/BlasterGame/Pickups/Pickup.h
@@ -0,0 +1,41 @@
+// (c) 2023 Will Roberts
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Pickup.generated.h"
+
+UCLASS()
+class BLASTERGAME_API APickup : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	APickup();
+	virtual void Tick(float DeltaTime) override;
+	virtual void Destroyed() override;
+
+protected:
+	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	virtual void OnSphereOverlap(
+		UPrimitiveComponent* OverlappedComponent,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex,
+		bool bFromSweep,
+		const FHitResult& SweepResult
+	);
+
+private:
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent* PickupMesh;
+
+	UPROPERTY(EditAnywhere)
+	class USphereComponent* OverlapSphere;
+
+	UPROPERTY(EditAnywhere)
+	class USoundCue* PickupSound;
+};
